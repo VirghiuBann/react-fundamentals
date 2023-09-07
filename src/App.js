97f85d41d2b9208ed378cdbad4d01e2b10a5bbcd
virghiu <virghiu@gmail.com>
2023-09-07 16:53:49 -0400

    feat: inline style

--- src/App.js
+++ src/App.js
@@ -36,6 +36,13 @@ const Image = () => (
 const Title = () => {
   return <h2>Holly Hardcover </h2>
 }
-const Author = () => <h4> Stephen King</h4>
+const Author = () => {
+  const inlineHeadingStyles = {
+    color: '#617d98',
+    fontSize: '0.75rem',
+    marginTop: '0.5rem',
+  }
+  return <h4 style={inlineHeadingStyles}> Stephen King</h4>
+}
 
 export default App
