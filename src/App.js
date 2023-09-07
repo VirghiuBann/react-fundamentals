f58d912b750c3ff3437c23d8364b6adcf7f27410
virghiu <virghiu@gmail.com>
2023-09-07 17:44:29 -0400

    feat: children props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-const author = 'Stephen King'
-const title = 'Holly Hardcover'
-const img = './images/book-1.jpg'
+const firstBook = {
+  author: 'Stephen King',
+  title: 'Holly Hardcover',
+  img: 'https://m.media-amazon.com/images/I/417kSOgt4aL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
+}
+
+const secondBook = {
+  author: 'Rebecca Yarros',
+  title: 'Iron Flame',
+  img: 'https://m.media-amazon.com/images/I/51xSijRxXqL._SX307_BO1,204,203,200_.jpg',
+}
 
 function App() {
   return (
@@ -13,18 +21,30 @@ function App() {
 const BookList = () => {
   return (
     <section className='booklist'>
-      <Book author={author} title={title} img={img} />
-      <Book author={author} title={title} img={img} />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      >
+        {' '}
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit.{' '}
+      </Book>
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
     </section>
   )
 }
 
-const Book = ({ author, title, img }) => {
+const Book = ({ author, title, img, children }) => {
   return (
     <article className='book'>
       <img src={img} alt={author} />
       <h2>{title} </h2>
       <h4>{author}</h4>
+      <p>{children}</p>
     </article>
   )
 }
