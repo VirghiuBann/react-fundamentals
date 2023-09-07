6bcb52fece4bc1a3d599f339221ec55e931a9447
virghiu <virghiu@gmail.com>
2023-09-07 16:57:31 -0400

    feat: refactoring components

--- src/App.js
+++ src/App.js
@@ -20,29 +20,14 @@ const BookList = () => {
 const Book = () => {
   return (
     <article className='book'>
-      <Image />
-      <Title />
-      <Author />
+      <img
+        src='https://images-na.ssl-images-amazon.com/images/I/815oQ6G6HDL._AC_UL600_SR600,400_.jpg'
+        alt='Holly Hardcover '
+      />
+      <h2>Holly Hardcover </h2>
+      <h4> Stephen King</h4>
     </article>
   )
-}
-
-const Image = () => (
-  <img
-    src='https://images-na.ssl-images-amazon.com/images/I/815oQ6G6HDL._AC_UL600_SR600,400_.jpg'
-    alt='Holly Hardcover '
-  />
-)
-const Title = () => {
-  return <h2>Holly Hardcover </h2>
-}
-const Author = () => {
-  const inlineHeadingStyles = {
-    color: '#617d98',
-    fontSize: '0.75rem',
-    marginTop: '0.5rem',
-  }
-  return <h4 style={inlineHeadingStyles}> Stephen King</h4>
 }
 
 export default App
