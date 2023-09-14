a33fe5526494092eadab55f8a8605198bb92264d
virghiu <virghiu@gmail.com>
2023-09-14 17:33:34 -0400

    feat: number added in each book

--- src/App.js
+++ src/App.js
@@ -13,42 +13,42 @@ const BookList = () => {
   return (
     <section className='booklist'>
       {/* <EventExamples /> */}
-      {books.map((book) => {
-        return <Book {...book} key={book.id} />
+      {books.map((book, index) => {
+        return <Book {...book} key={book.id} number={index} />
       })}
     </section>
   )
 }
 
-const EventExamples = () => {
-  const handleFormInput = () => {
-    console.log('handle form input')
-  }
+// const EventExamples = () => {
+//   const handleFormInput = () => {
+//     console.log('handle form input')
+//   }
 
-  const handleButtonClick = () => {
-    alert('handle button click')
-  }
+//   const handleButtonClick = () => {
+//     alert('handle button click')
+//   }
 
-  const handleFromSubmission = (e) => {
-    e.preventDefault()
-    console.log('form submission')
-  }
+//   const handleFromSubmission = (e) => {
+//     e.preventDefault()
+//     console.log('form submission')
+//   }
 
-  return (
-    <section>
-      <form onSubmit={handleFromSubmission}>
-        <h2>Typical Form</h2>
-        <input
-          type='text'
-          name='example'
-          onChange={handleFormInput}
-          style={{ margin: '1rem 0' }}
-        />
-        <button type='submit'>submit form</button>
-      </form>
-      <button onClick={handleButtonClick}>click me</button>
-    </section>
-  )
-}
+//   return (
+//     <section>
+//       <form onSubmit={handleFromSubmission}>
+//         <h2>Typical Form</h2>
+//         <input
+//           type='text'
+//           name='example'
+//           onChange={handleFormInput}
+//           style={{ margin: '1rem 0' }}
+//         />
+//         <button type='submit'>submit form</button>
+//       </form>
+//       <button onClick={handleButtonClick}>click me</button>
+//     </section>
+//   )
+// }
 
 export default App
--- src/Book.js
+++ src/Book.js
@@ -1,10 +1,11 @@
-const Book = ({ id, author, title, img }) => {
+const Book = ({ id, author, title, img, number }) => {
   return (
     <article className='book'>
       <img src={img} alt={author} />
       <h2>{title} </h2>
 
       <h4>{author}</h4>
+      <span className='number'>{`# ${number + 1}`}</span>
     </article>
   )
 }
